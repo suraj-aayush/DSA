3ad7ff117c423a3241a2a3426f2ea349bc22c934
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2022-12-31 00:47:34 +0530

    Add files via upload

---
+++ Queue/queue.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define max 5
+int que[max],f=-1,r=-1;
+void enq();
+void deq();
+void display();
+
+int main()
+{
+    int ch;
+    printf("1. insert rear \n");
+    printf("2. delete front \n");
+    printf("3. display \n");
+    printf("4. exit \n");
+    while(1){
+        printf("enter choice \n");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1: enq();
+            break;
+            
+            case 2: deq();
+            break;
+            
+            case 3: display();
+            break;
+            
+            case 4: exit(0);
+            break;
+            
+            default:
+            printf("wrong choice \n");            
+        }
+    }
+    return 0;
+}
+
+void enq(){
+    int item;
+    
+    if (r==max-1)
+    {
+        printf("queue is full \n");
+    }
+    else
+    {
+        if(f==-1)
+        {
+            f=0;
+        }
+        
+        printf("enter ur element \n");
+        scanf("%d",&item);
+        r=r+1;
+        que[r]=item;
+    }
+    
+}
+
+void deq(){
+    if (f==-1)
+    {
+        printf("stack empty \n");
+    }
+    else
+    {
+        printf("removed element is %d \n",que[f]);
+        f=f+1;
+    }
+}
+
+
+void display(){
+    int i;
+    if (f==-1)
+    {
+        printf("queue is empty ");
+    }
+    
+    else
+    {
+        for(i=f;i<=r;i++)
+        {
+            printf("%d \n",que[i]);
+        }
+    }
+}
